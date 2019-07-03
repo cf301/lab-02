5391566afeb6bbcd87dae2fce2e033fc9e42dfdd
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-03 11:37:04 -0700

    evil plan to complete feature two DONE DONE PP bomibear

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ Actual time needed to complete: 23 mins
 Number and name of feature: Filter images
 Estimate of time needed to complete: 80 min
 Start time: 10:05am
-Finish time: 
-Actual time needed to complete: 
+Finish time: 11:35am
+Actual time needed to complete: 1 hour and 30 mins
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
-
+//array to hold all creatures
 const allCreatures = [];
-
+//constructor
 const Creature = function(image_url, title, description, keyword, horns) {
   this.image_url = image_url;
   this.title = title;
@@ -10,60 +10,55 @@ const Creature = function(image_url, title, description, keyword, horns) {
   this.horns = horns;
   allCreatures.push(this);
 };
-
+//added render function
 Creature.prototype.renderWithJQuery = function() {
   const $newCreature = $('<section></section');
 
+  //find our place in the DOM
   const creatureTemplateHTML = $('#photo-template').html();
-
   $newCreature.html(creatureTemplateHTML);
-
+  //populate these tags
   $newCreature.find('h2').text(this.title);
   $newCreature.find('img').attr('src', this.image_url);
   $newCreature.find('p').text(this.description);
-
+  //append
   $('main').append($newCreature);
 };
 
 Creature.getCreaturesFromFile = function() {
   const filePath = 'data/page-1.json';
   const fileType = 'json';
+  //aysnc call to instantiate new creatures
   $.get(filePath, fileType).then( creatureJSON => {
+    //iterate
     creatureJSON.forEach( item => {
       new Creature(item.image_url, item.title, item.description, item.keyword, item.horns);
     });
+    //populate our drop down with all creatures
+    populateDropDown();
+    //iterate
     allCreatures.forEach(item => {
+      //render each picture
       item.renderWithJQuery();
+      
     });
   });
 };
 
-Creature.getCreaturesFromFile();
 
 function populateDropDown() {
-
-  //select our dropdown object
-  var $target = $('select')[0];
-  console.log($target);
-  //populate by keyword 
-  allCreatures.forEach(function(item) {
+  //iterate
+  allCreatures.forEach( item => {
     //new option object in HTML
-    // var o = new Option(item.keyword, item.keyword);
+    var o = new Option(item.keyword, item.keyword);
     //actual html text
-    // $(o).html(item.keyword);
+    $(o).html(item.keyword);
     //finding our select HTml tag, and appending our new option
-    // $('select').append(o);
-
-    const $option = $('<option>dummy text</option>');
-    // const selectTemplate = $('select').html();
-    // $option.html(selectTemplate);
-    // $option.find('option').text('hardcoded text');
-    $('select')[0].append('<option>dummy text</option>');
+    $('select').append(o);
   });
-
 }
 
 $(document).ready(function () {
-  //can we get an option inside the select
-  populateDropDown();
+  //grab our creatures from file, but not until page loads
+  Creature.getCreaturesFromFile();
 })
